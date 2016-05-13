9a00069fa74b769f6630de74824c360c8eb88a4d
Buck & Mila & Jonathan Nicolas & Renan Martins <pair+buckmelton.ftBessmann.jonathanNicolas.nbkhope@devbootcamp.com>
2016-05-12 18:25:21 -0700

    Add migration to create users, votes, answers, and comments tables

--- db/migrate/20160512163152_create_answers.rb
+++ db/migrate/20160512163152_create_answers.rb
@@ -1,4 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
+      create_table :answers do |t|
+      t.string :content
+      t.belongs_to :user_id, index: true
+      t.belongs_to :question_id, index: true
+
+      t.timestamps null: false
+    end
   end
 end
--- db/migrate/20160512163159_create_comments.rb
+++ db/migrate/20160512163159_create_comments.rb
@@ -1,4 +1,18 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.string :content, null: false
+      t.belongs_to :user_id, index: true
+
+      t.integer :entry_id, null: false
+      t.string :entry_type, null: false
+      # You could also do:
+      # t.references :entry, polymorphic: true, index: true
+
+      t.timestamps null: false
+    end
+
+    add_index :comments, :entry_id
+
   end
 end
