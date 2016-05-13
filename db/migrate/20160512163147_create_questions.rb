f702cc72f63f5a1846160373fd717dfdc7a79197
Buck & Mila & Jonathan Nicolas & Renan Martins <pair+buckmelton.ftBessmann.jonathanNicolas.nbkhope@devbootcamp.com>
2016-05-12 18:18:03 -0700

    Define migration to create questions table

--- db/migrate/20160512163147_create_questions.rb
+++ db/migrate/20160512163147_create_questions.rb
@@ -1,4 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string :title
+      t.string :content
+      t.belongs_to :user_id, index: true
+
+      t.timestamps null: false
+    end
   end
 end
