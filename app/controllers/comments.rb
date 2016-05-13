f39c3d167e131aaee09b72ddf0a3073e1761b12e
Buck & Mila & Jonathan Nicolas & Renan Martins <pair+buckmelton.ftbessmann.jonathanNicolas.nbkhope@devbootcamp.com>
2016-05-13 16:50:15 -0700

    Add spacing between lines

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -10,16 +10,20 @@ end
 
 post '/questions/:id/comments' do
   @question = Question.find_by(id: params[:id])
+
   comment = Comment.new(params[:comment])
   comment.user = current_user
   @question.comments << comment
+
   redirect "/questions/#{@question.id}"
 end
 
 post '/answers/:id/comments' do
   @answer = Answer.find_by(id: params[:id])
+
   comment = Comment.new(params[:comment])
   comment.user = current_user
   @answer.comments << comment
+
   redirect "/questions/#{@answer.question.id}"
 end
