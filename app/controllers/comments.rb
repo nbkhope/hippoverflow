27eb9dcc2f309000e3845da221b47468998c9d31
Buck <buck9063@gmail.com>
2016-05-13 16:19:14 -0700

    Add routes and views for questions, answers, and comments

---
+++ app/controllers/comments.rb
@@ -0,0 +1,25 @@
+get '/questions/:id/comments/new' do
+  @question = Question.find_by(params[:id])
+  erb :'comments/new'
+end
+
+get '/answers/:id/comments/new' do
+  @answer = Answer.find_by(id: params[:id])
+  erb :'comments/new'
+end
+
+post '/questions/:id/comments' do
+  @question = Question.find_by(id: params[:id])
+  comment = Comment.new(params[:comment])
+  comment.user = current_user
+  @question.comments << comment
+  redirect "/questions/#{@question.id}"
+end
+
+post '/answers/:id/comments' do
+  @answer = Answer.find_by(id: params[:id])
+  comment = Comment.new(params[:comment])
+  comment.user = current_user
+  @answer.comments << comment
+  redirect "/questions/#{@answer.question.id}"
+end
